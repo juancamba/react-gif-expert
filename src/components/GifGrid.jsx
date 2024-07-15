7e30c282dacbaa4ca511a95e4674311a33c48542
Juan Camba <juan_camba@yahoo.com.ar>
2024-07-15 17:39:28 +0200

    end 85

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,22 @@
 
 import {getGifs} from '../helpers/getGifs';
+import { useEffect, useState } from 'react';
  
 export const GifGrid = ({category}) => {
  
+  
+  
+  useEffect(() => {
     getGifs(category);
-  return (
-    <>
-        <h3>{category}</h3>
-        <p>Hola</p>
-    </>
-  )
+   
+  }, [])
+  
+
+  
+    return (
+      <>
+          <h3>{category}</h3>
+       
+      </>
+    )
 }
