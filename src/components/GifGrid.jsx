6d415b3ab9a81f3335e0b27415bef127586a678a
Juan Camba <juan_camba@yahoo.com.ar>
2024-07-17 17:33:53 +0200

    end 89 90 archivos de barril

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -15,6 +15,10 @@ export const GifGrid = ({category}) => {
     return (
       <>
         <h3>{category}</h3>
+        {
+            //isLoading  ? (<h2>Cargando</h2>) : null
+            isLoading  && (<h2>Cargando</h2>) 
+        }
         <div className='card-grid'>
           {
             images.map( (image) => (
