0babb39760c9020c34fbfea7b03c497477500188
Juan Camba <juan_camba@yahoo.com.ar>
2024-07-17 19:16:26 +0200

    last changes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) =>{
         
-    const url =`https://api.giphy.com/v1/gifs/search?api_key=gqUyAwQ0NqQNyzMizfALutMxKdNBuuqf&q=${category}&limit=20`;
+    const url =`https://api.giphy.com/v1/gifs/search?api_key=gqUyAwQ0NqQNyzMizfALutMxKdNBuuqf&q=${category}&limit=3`;
 
     const resp = await fetch(url);
 
